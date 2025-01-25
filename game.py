4c3ebcb514ae0909072de380166da20bfefc7e34
nishok2913 <nishoknishok269@gmail.com>
2025-01-25 18:41:37 +0530

    one feature added

--- game.py
+++ game.py
@@ -5,5 +5,14 @@ guess_number = int(input("Guess a random number from 1 to 10: "))
 
 if(random.choice(list1) == guess_number):
     print("You guessed it!")
-else:
-    print("You didn't guess it!")
+elif(random.choice(list1) != guess_number):
+    if(random.choice(list1) < guess_number):
+        print("Too high!")
+    elif(random.choice(list1) > guess_number):
+        print("Too low!")
+    print("guess again")
+    guess_number1 = int(input("Guess the number: "))
+    if(random.choice(list1) == guess_number1):
+        print("You guessed it!")
+    else:
+        print("You lost!")
