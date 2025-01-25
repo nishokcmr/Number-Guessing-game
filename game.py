772f67c3484bbb73c82f16cf0f4e9ab998f50472
nishok2913 <nishoknishok269@gmail.com>
2025-01-25 19:04:37 +0530

    Number is printed at the end

--- game.py
+++ game.py
@@ -1,18 +1,18 @@
 import random
 
 list1 = [1,2,3,4,5,6,7,8,9,10]
+number = random.choice(list1)
 guess_number = int(input("Guess a random number from 1 to 10: "))
-
-if(random.choice(list1) == guess_number):
+if(number == guess_number):
     print("You guessed it!")
-elif(random.choice(list1) != guess_number):
-    if(random.choice(list1) < guess_number):
+elif(number != guess_number):
+    if(number < guess_number):
         print("Too high!")
-    elif(random.choice(list1) > guess_number):
+    elif(number > guess_number):
         print("Too low!")
     print("guess again")
     guess_number1 = int(input("Guess the number: "))
-    if(random.choice(list1) == guess_number1):
+    if(number == guess_number1):
         print("You guessed it!")
     else:
-        print("You lost!, Try again")
+        print("Nah, The number is " + str(number))
